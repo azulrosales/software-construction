94eaf68e798cdba59513f049063311811800e920
Azul Rosales <azul@MECHAGODZILLA.local>
2023-03-16 11:17:55 -0600

    Updated Lab 14

--- Lab14/app.js
+++ Lab14/app.js
@@ -8,24 +8,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
-app.set('views','views/partials');
+app.set('views', 'views/partials');
 
 app.use(express.static(path.join(__dirname)));
-
-const homeModule = require("./routes/home.routes");
-const buyModule = require("./routes/buy.routes");
-const newModule = require("./routes/new.routes");
-const userModule = require("./routes/user.routes");
-
-app.use('/', homeModule);
-app.use('/buy', buyModule);
-app.use('/new', newModule);
-app.use('/user', userModule);
 
 app.use(session({
     secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
     resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
     saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
 }));
+
+const homeModule = require("./routes/home.routes");
+app.use('/', homeModule);
+const buyModule = require("./routes/buy.routes");
+app.use('/buy', buyModule);
+const newModule = require("./routes/new.routes");
+app.use('/new', newModule);
+const userModule = require("./routes/user.routes");
+app.use('/user', userModule);
 
 app.listen(3000);
